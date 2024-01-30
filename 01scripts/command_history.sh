4e23d0f340e2b7fad997ee39c211ee4bc5fdb602
JuanWSL <juan.a.t.s2001@hotmail.es>
2024-01-30 21:08:34 +0100

    fastqc of short and long reads

--- 01scripts/command_history.sh
+++ 01scripts/command_history.sh
@@ -6,5 +6,16 @@ mkdir 00data
 mkdir 01scripts
 mv command_history.sh 01scripts/
 mv *fastq 00data/
+mkdir 02results
+mkdir 02results/fastqc_shorts
+mkdir 02results/fastqc_longs
+
+###### PREGUNTA 1. ######
+# FASTQC (short reads)
+ fastqc 00data/bs_illumina_R?.fastq -o 02results/fastqc_shorts/
+# Trimmomatic
 
 
+
+# FASTQC (long reads)
+fastqc 00data/bs_reads_nanopore.fastq -o 02results/fastqc_longs/
