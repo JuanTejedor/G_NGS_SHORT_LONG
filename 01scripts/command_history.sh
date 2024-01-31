7448ec4c9e41f2b3d10bf8226b7c747df9cc9847
JuanWSL <juan.a.t.s2001@hotmail.es>
2024-01-31 15:41:58 +0100

    ensamblaje de long reads con Flye

--- 01scripts/command_history.sh
+++ 01scripts/command_history.sh
@@ -16,11 +16,8 @@ mkdir 03final_reports
 mkdir 02results/fastqc_shorts
 fastqc 00data/bs_illumina_R?.fastq -o 02results/fastqc_shorts/
 
-
 # Trimmomatic
-
 mkdir 02results/trimmomatic
-
 trimmomatic PE \
 -trimlog 02results/trimmomatic/log.txt \
 -threads 6 \
@@ -40,7 +37,20 @@ MINLEN:50
 # FASTQC (short reads) (POST)
 fastqc 02results/trimmomatic/trimmed_1P.fastq 02results/trimmomatic/trimmed_2P.fastq -o 02results/fastqc_shorts/
 
+
+
+###### PREGUNTA 2. ######
 # FASTQC (long reads)
 mkdir 02results/fastqc_longs
 fastqc 00data/bs_reads_nanopore.fastq -o 02results/fastqc_longs/
 
+###### PREGUNTA 3. ######
+# ENSAMBLAJE FLYE (long reads)
+mkdir 02results/flye
+flye \
+--nano-raw 00data/bs_reads_nanopore.fastq \
+-g 4m \
+-i 1 \
+-t 6 \
+-o 02results/flye
+# ruta ensamblaje: 02results/flye/assembly.fasta
